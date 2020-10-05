ea7e6cc83bdaeff99c6f45a61415bfc801882225
SIDDHARTH SAHOO <sahoosiddharth3@gmail.com>
2020-10-05 23:53:01 +0530

    Add book functionality added with event Listner

--- script/script.js
+++ script/script.js
@@ -1,3 +1,7 @@
+const addBookButton = document.querySelector('#add-book');
+const bookformSelector = document.querySelector('#book-form');
+
+
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -7,6 +11,14 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-function addBookToLibrary() {
+addBookButton.addEventListener('click',function(e){
+  bookformSelector.style.display = 'flex';
+  e.target.style.display = 'none';
+})
 
+function addBookToLibrary() {
+  const titleSelector = document.querySelector("#title").value;
+  const authorSelector = document.querySelector('#author').value;
+  const pagesSelector = document.querySelector('#pages').value;
+  const readSelector = document.querySelector('#read').value;
 }
