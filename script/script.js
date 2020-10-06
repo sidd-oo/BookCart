7bdbdf134fde669d64ac81af95efa14ca142a080
SIDDHARTH SAHOO <sahoosiddharth3@gmail.com>
2020-10-06 23:58:47 +0530

    Pushing newBook obj into the array

--- script/script.js
+++ script/script.js
@@ -1,7 +1,6 @@
-const addBookButton = document.querySelector('#add-book');
-const bookformSelector = document.querySelector('#book-form');
-
-
+const newBookButton = document.querySelector('#add-book');
+const bookForm = document.querySelector('#book-form');
+const submitForm = document.querySelector('#submit-btn');
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -11,14 +10,33 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-addBookButton.addEventListener('click',function(e){
-  bookformSelector.style.display = 'flex';
-  e.target.style.display = 'none';
-})
-
-function addBookToLibrary() {
-  const titleSelector = document.querySelector("#title").value;
-  const authorSelector = document.querySelector('#author').value;
-  const pagesSelector = document.querySelector('#pages').value;
-  const readSelector = document.querySelector('#read').value;
+function showForm(event){
+  bookForm.style.display = 'flex';
+  event.target.style.display = 'none';
 }
+function hideForm(event){
+  bookForm.style.display = 'none';
+  newBookButton.style.display = '';
+}
+
+function addBookToLibrary(event) {
+    event.preventDefault();
+    const title = document.querySelector("#title").value;
+    const author = document.querySelector('#author').value;
+    const pages = document.querySelector('#pages').value;
+    const isRead = document.querySelector('#read').checked;
+
+    if(title == "" || author == "" || pages == ""){
+      alert("Please fill the empty fields")
+    }else if(pages == 0){
+      alert("Pages of book can't be zero. Try filling the field again!")
+    }else{
+      let bookObj = new Book(title, author, pages, isRead);
+      myLibrary.push(bookObj);
+    }
+
+    hideForm();
+}
+
+newBookButton.addEventListener('click',showForm);
+submitForm.addEventListener('click',addBookToLibrary);
